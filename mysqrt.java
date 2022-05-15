ea41c2bdc0e3d9bbfe3968881749fc2e0c6ef7e3
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-05-15 16:46:59 +0530

    Update mysqrt.java

--- mysqrt.java
+++ mysqrt.java
@@ -1,7 +1,7 @@
 public static int mySqrt(int x) { // Babylonian Method
         if (x == 1)
             return 1;
-        double guess = x / 2;
+        double guess = x / 2; // Using number/2 for approximation
         double byNumber = x / guess;
         double avg = (guess + byNumber) / 2;
 
