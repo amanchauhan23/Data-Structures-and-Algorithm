9327bc19393f475f864d0eebd7d945d220947c6e
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-05-18 23:23:31 +0530

    Update removeDuplicates.java

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -1,19 +1,32 @@
 public ListNode deleteDuplicates(ListNode head) { // O(N)
-        if(head==null)
-            return head;
-        ListNode a = head;
-        while(a.next != null){
-            ListNode t = a.next;
-            while(a.val == t.val){
+//         #1
+//         if(head==null)
+//             return head;
+//         ListNode a = head;
+//         while(a.next != null){
+//             ListNode t = a.next;
+//             while(a.val == t.val){
+//                 t = t.next;
+//                 if(t==null){
+//                     a.next = null;
+//                     return head;
+//                 }
+//                 continue;
+//             }
+//             a.next = t;
+//             a = a.next;
+//         }
+//         return head;
+        
+//      #2
+        ListNode t = head;
+        while(t!=null){
+            if(t.next==null)
+                break;
+            if(t.val==t.next.val)
+                t.next= t.next.next;
+            else
                 t = t.next;
-                if(t==null){
-                    a.next = null;
-                    return head;
-                }
-                continue;
-            }
-            a.next = t;
-            a = a.next;
         }
         return head;
     }
