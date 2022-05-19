52b145fb5bec13ee59b4d8b584e8ca69adade6d3
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-05-19 23:13:54 +0530

    Update mergeSortedArray.java

--- mergeSortedArray.java
+++ mergeSortedArray.java
@@ -2,5 +2,5 @@ public void merge(int[] nums1, int m, int[] nums2, int n) {
        for (int i = 0; i < n; i++) {
             nums1[m+i] = nums2[i];
         }
-        Arrays.sort(nums1);
+        Arrays.sort(nums1); // O(m+n log(m+n))
     }
