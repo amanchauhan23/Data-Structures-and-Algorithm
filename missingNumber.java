b8c4354a296b6a93c8c8c64d2f6e33ea0bf989fe
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-05-29 00:34:23 +0530

    Update missingNumber.java

--- missingNumber.java
+++ missingNumber.java
@@ -1,7 +1,7 @@
 public int missingNumber(int[] nums) {
         int sum=0;
         int n = nums.length;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             sum+=nums[i];
         }
         return n*(n+1)/2 - sum ;
