d8faf239cc504f6154f392d50015e3e779e12f43
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-06-01 00:00:07 +0530

    Create CheckIfaStringContainsAllBinaryCodesofSizeK.java

---
+++ CheckIfaStringContainsAllBinaryCodesofSizeK.java
@@ -0,0 +1,20 @@
+public boolean hasAllCodes(String s, int k) {
+  
+        // #1 
+        // HashSet<String> hs = new HashSet<>();
+        // int distinct = 1 << k; // 2^k distinct code for length k, as every bit can be 0 or 1
+        // for (int i = 0; i < s.length() - k + 1; i++) {
+        //     String sub = s.substring(i, i + k);
+        //     if (!hs.contains(sub)) {
+        //         hs.add(sub);
+        //         distinct--;
+        //         if (distinct == 0)
+        //             return true;
+        //     }
+        // }
+        // return false;
+
+        // #2
+              
+  
+    }
