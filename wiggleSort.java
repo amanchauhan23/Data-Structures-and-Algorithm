b15fab6614b25cc308d6221b4125ea7fd657ce3a
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-06-03 00:11:08 +0530

    Update wiggleSort.java

--- wiggleSort.java
+++ wiggleSort.java
@@ -1,15 +1,11 @@
-public void wiggleSort(int[] nums) {
-        // write your code here
-        for(int i = 1; i< nums.length;i++){
-            if(i%2==0 && nums[i]>nums[i-1]){
-                int temp = nums[i];
-                nums[i] = nums[i-1];
-                nums[i-1] = temp;
-            }
-             if(i%2==1 && nums[i]<nums[i-1]){
-                 int temp = nums[i];
-                nums[i] = nums[i-1];
-                nums[i-1] = temp;
+public static void wiggleSort(int[] arr) { // O(N) | O(1) 
+    for(int i = 1; i< arr.length;i++){
+            if((i%2==0 && arr[i]>arr[i-1])||(i%2==1 && arr[i]<arr[i-1])){
+                int temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = temp;
             }
         }
+    
+  }
     }
