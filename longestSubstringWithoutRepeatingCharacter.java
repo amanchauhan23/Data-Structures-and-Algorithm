25b6e686941a946110f93bcad4c6dd0875c389f0
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-06-09 12:53:09 +0530

    Update longestSubstringWithoutRepeatingCharacter.java

--- longestSubstringWithoutRepeatingCharacter.java
+++ longestSubstringWithoutRepeatingCharacter.java
@@ -3,6 +3,7 @@ import java.util.HashSet;
 public class longestSubstringWithoutRepeatingCharacter {
 
     public int lengthOfLongestSubstring(String s) {
+        // #1
         int ans = 0;
         int left = 0;
         int right = 0;
@@ -14,6 +15,23 @@ public class longestSubstringWithoutRepeatingCharacter {
                 ans = Math.max(ans, hset.size());
             } else {
                 hset.remove(s.charAt(left++));
+            }
+        }
+
+        return ans;
+        
+        // #2
+        int ans = 0;
+        int left = 0;
+        int right = 0;
+        char[] ch = new char[256];
+        while (right < s.length()) {
+            if (ch[s.charAt(right)]==0) {
+                ch[s.charAt(right)]++;
+                ans = Math.max(ans, right-left+1);
+                right++;
+            } else {
+                ch[s.charAt(left++)]--;
             }
         }
 
