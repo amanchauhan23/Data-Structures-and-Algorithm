4c7b4aa7d7c0e765fa6cf608b4a38fc5db2db5e3
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-06-14 15:25:59 +0530

    Create linkedListCycleTwo.java

---
+++ linkedListCycleTwo.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) { // O(n) | O(1)
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        boolean b = true;
+        while(fast != null && fast.next != null){ // floyd's cycle detection
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                b = false;
+                break;
+            }
+        }
+        
+        if(b)
+            return null;
+        
+        slow = head;
+        
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+    }
+}
