3da776045e688690b3ed449148034704d5da46ce
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-06-16 10:25:49 +0530

    Update inorderTraversal.java

--- inorderTraversal.java
+++ inorderTraversal.java
@@ -1,4 +1,22 @@
     public List<Integer> inorderTraversal(TreeNode root) {
+        
+        // iterative 
+//         List<Integer> ls = new ArrayList<>();
+//         Stack<TreeNode> s = new Stack<>();
+        
+//         TreeNode cur = root;
+        
+//         while(cur != null || !s.isEmpty()){
+//             while(cur != null){
+//                 s.push(cur);
+//                 cur = cur.left;
+//             }
+//             TreeNode temp = s.pop();
+//             ls.add(temp.val);
+//             cur = temp.right;
+//         }
+//         return ls;
+        
         List<Integer> l = new ArrayList<>();
         helper(root, l);
         return l;
