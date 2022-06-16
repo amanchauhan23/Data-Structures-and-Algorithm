26251cc3e267fbadb529781fd72e083a6b9dea93
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-06-16 10:29:52 +0530

    Update BinaryTreePreorderTraversal.java

--- BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal.java
@@ -1,4 +1,23 @@
 class Solution {
+    
+    // iterative
+//     List<Integer> ls = new ArrayList<>();
+//         Stack<TreeNode> s = new Stack<>();
+        
+//         TreeNode cur = root;
+        
+//         while(cur != null || !s.isEmpty()){
+//             while(cur != null){
+//                 ls.add(cur.val);
+//                 s.push(cur);
+//                 cur = cur.left;
+//             }
+//             TreeNode temp = s.pop();
+//             cur = temp.right;
+//         }
+//         return ls;
+    
+    // recursive
     List<Integer> ls = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
         help(root);
