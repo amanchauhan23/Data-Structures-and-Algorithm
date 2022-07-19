47303bd99baea0436d52afd390b6d36b1832562c
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-07-19 23:59:19 +0530

    Create MaximumSumIncreasingSubsequence.java

---
+++ MaximumSumIncreasingSubsequence.java
@@ -0,0 +1,18 @@
+class Solution
+{
+	public int maxSumIS(int arr[], int n)  // O(n^2) | O(n)
+	{ 
+	    int msis[] = new int[n];
+	    for(int i = 0; i < n; i++){
+	        msis[i] = arr[i];
+	        for(int j = 0; j < i; j++){
+	            if(arr[j] < arr[i])
+	                msis[i] = Math.max(msis[i], arr[i] + msis[j]);
+	        }
+	    }
+	    int ans = Integer.MIN_VALUE;
+	    for(int i : msis)
+	        ans = Math.max(i, ans);
+	    return ans;
+	}  
+}
