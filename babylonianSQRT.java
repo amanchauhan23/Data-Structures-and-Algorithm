757630cc2690177a5cfb9065acc90ad1b15fc99f
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-07-22 23:59:47 +0530

    Update babylonianSQRT.java

--- babylonianSQRT.java
+++ babylonianSQRT.java
@@ -3,7 +3,7 @@ public static double sqrt(float n) {
         float x = n;
         float y = 1;
 
-        // e decides the accuracy level
+        // e decides the accuracy level of sqrt cal
         double e = 0.000001;
         while (Math.abs(x - y) > e) {
             x = (x + y) / 2;
