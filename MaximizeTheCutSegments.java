16a0722c4d43f0448460be5df9cb1181abf16265
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-07-27 12:44:29 +0530

    Update MaximizeTheCutSegments.java

--- MaximizeTheCutSegments.java
+++ MaximizeTheCutSegments.java
@@ -54,4 +54,28 @@ class Solution
 }
 
 
-// #3 
+// #3 tabulation
+class Solution
+{
+    //Function to find the maximum number of cuts.
+    public int maximizeCuts(int n, int x, int y, int z)
+    {
+       
+       int dp[] = new int[n+1];
+       Arrays.fill(dp, Integer.MIN_VALUE);
+       
+       dp[0] = 0;
+       
+       for(int i = 1; i <= n; i++){
+            if(i - x >= 0)
+                dp[i] = Math.max(dp[i], dp[i - x] + 1);
+            if(i - y >= 0)
+                dp[i] = Math.max(dp[i], dp[i - y] + 1);
+            if(i - z >= 0)
+                dp[i] = Math.max(dp[i], dp[i - z] + 1);
+       }
+       if(dp[n] < 0)
+            return 0;
+       return dp[n];
+    }
+}
