7e0651db8c51869c54c64819ee5fbc4d9ab49c0f
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-08-06 04:58:26 -0700

    Update SuperEggDrop.java

--- SuperEggDrop.java
+++ SuperEggDrop.java
@@ -48,3 +48,48 @@ class Solution {
     }
     
 }
+
+// #3 Tabulation
+
+
+// #4 optimized binary search + memo ||| Accepted
+class Solution { // O(k*NlogN) || O(kN)
+    int memo[][];
+    public int superEggDrop(int k, int n) {
+        memo = new int[k+1][n+1];
+        return egg(k,n);
+    }
+    
+    public int egg(int k, int n){
+        if(n == 0)
+            return 0;
+        if(k == 1)
+            return n;
+        
+        int low = 1;
+        int high = n;
+        
+        if(memo[k][n] != 0)
+            return memo[k][n];
+        
+        int ans = Integer.MAX_VALUE;
+        while(low <= high){
+            int mid = (low + high)/2;
+            int tone = egg(k-1, mid-1);
+            int ttwo = egg(k, n-mid);
+            
+            if(tone < ttwo){
+                low = mid + 1;
+                ans = Math.min(ttwo + 1, ans);
+            }else if(tone > ttwo){
+                high = mid - 1;
+                ans = Math.min(tone + 1, ans);
+            }else{
+                ans = Math.min(ttwo + 1, ans);
+                break;
+            }
+        }
+        memo[k][n] = ans;
+        return memo[k][n];
+    }
+}
