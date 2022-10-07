79ce448cd1c8adc2a95074f6b06893856533c76a
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-10-07 21:37:06 +0530

    Create 0-1Matrix.java

---
+++ 0-1Matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public int[][] updateMatrix(int[][] mat) { // O(n*m) || O(n*m) || in-place BFS
+       int dist[][] = new int[][]{
+           {0,1}, {1, 0},
+           {0, -1}, {-1, 0}
+       };
+
+       int l = mat.length;
+       int w = mat[0].length;
+       int result[][] = new int[l][w];
+       Queue<int []> q = new LinkedList<>();
+
+       for(int i = 0; i < l; i++){
+           for(int j = 0; j < w; j++){
+               if(mat[i][j] == 0){
+                  q.add(new int[]{i, j});
+               }else{
+                   mat[i][j] = -1;
+               }
+           }
+        } 
+
+        int level = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            level++;
+            while(size-- > 0){
+                int pop[] = q.poll();
+                int row = pop[0];
+                int col = pop[1];
+
+                for(int [] arr : dist){
+                    int nrow = row + arr[0];
+                    int ncol = col + arr[1];
+                    if(nrow < 0 || ncol < 0 || nrow == l || ncol == w || mat[nrow][ncol] != -1) continue;
+                    mat[nrow][ncol] = level;
+                    q.offer(new int[]{nrow, ncol});
+                }
+            }
+        }
+
+        return mat;
+    }
+
+}
