c26cf77b4a70540f8162079309e8f76cf4bf0810
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-10-10 02:44:40 +0530

    Create Maximum69Number.java

---
+++ Maximum69Number.java
@@ -0,0 +1,21 @@
+// NICE
+// #1 
+class Solution {
+    public int maximum69Number (int num) {
+        return Integer.parseInt((""+num).replaceFirst("6", "9"));
+    }
+}
+
+// #2
+class Solution {
+    public int maximum69Number (int num) {
+        char [] nums = String.valueOf(num).toCharArray();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == '6'){
+                nums[i] = '9'; 
+                return Integer.parseInt(String.valueOf(nums));
+            }
+        }
+        return Integer.parseInt(String.valueOf(nums));
+    }
+}
