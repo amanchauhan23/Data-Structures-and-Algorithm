e94c4d500bfebe1690461842a0d487b2316359f9
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-10-09 21:46:23 +0530

    Create CountPrimes.java

---
+++ CountPrimes.java
@@ -0,0 +1,28 @@
+// #1 || O(n log log n) || O(n)
+class Solution {
+    public int countPrimes(int n) {
+        if(n < 2) return 0;
+        boolean sieve[] = new boolean[n];
+        int ans = 0;
+        for(int i = 2; i*i < n; i++){
+            if(isPrime(i)){
+                for(int j = i*2; j < n; j = j + i){
+                    sieve[j] = true;
+                }
+            }
+        }
+
+        for(int i = 2; i < n; i++){
+            if(!sieve[i]) ans++;
+        }
+        
+        return ans;
+    }
+
+    private boolean isPrime(int num){
+        int upper = (int)Math.sqrt(num);
+        for(int i = 2; i <= upper; i++)
+            if(num%i == 0) return false;
+        return true;
+    }
+}
