5d0a05b9765a50a5577c062bc07f75acc1a53dbe
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-10-21 20:02:19 +0530

    Update MinimumHeightTree.java

--- MinimumHeightTree.java
+++ MinimumHeightTree.java
@@ -42,3 +42,55 @@ class Solution {
         return ls;
     }
 }
+
+// #2 || Max optimal root can be two. || we begin with the leaf nodes and then trim inwards, untill we are left with one or two nodes.
+class Solution {
+
+    List<List<Integer>> adj;
+    
+    public void createAdj(int n, int[][] edges, int [] degree){ 
+        adj = new ArrayList<>(n);
+        for(int i = 0; i < n; i++)
+            adj.add(new ArrayList<Integer>());
+        for(int i = 0; i < edges.length; i++){
+            degree[edges[i][0]]++;
+            degree[edges[i][1]]++;
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+    }
+
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> ls = new ArrayList<>();
+        if(n < 2){
+            ls.add(0);
+            return ls;
+        }
+        int degree[] = new int[n];
+        createAdj(n, edges, degree);
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++)
+            if(degree[i] == 1) q.offer(i);
+
+        int remainingNode = n;
+        while(remainingNode > 2){
+            remainingNode -= q.size();
+            int size = q.size();
+            while(size-- > 0){
+                int pop = q.poll();
+                for(int i = 0; i < adj.get(pop).size(); i++){
+                    int neighbour = adj.get(pop).get(i);
+                    degree[neighbour]--;
+                    if(degree[neighbour] == 1)
+                        q.offer(neighbour);
+                }
+            }
+        }
+
+        while(!q.isEmpty())
+            ls.add(q.poll());
+        
+        return ls;
+    }
+}
