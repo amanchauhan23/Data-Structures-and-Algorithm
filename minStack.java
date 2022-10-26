d524f1a44d225fc5f97bfda83345c5bffd079840
Aman Kumar Chauhan <63445788+amanchauhan23@users.noreply.github.com>
2022-10-27 00:35:47 +0530

    Update minStack.java

--- minStack.java
+++ minStack.java
@@ -1,32 +1,4 @@
-class MinStack { // #1
-     Stack<Integer> st;
-    Stack<Integer> minst;
-    public MinStack() {
-        st = new Stack<Integer>();
-        minst = new Stack<Integer>();
-    }
-    
-    public void push(int val) {
-         st.push(val);
-        if(minst.size()==0 || getMin()>=val)
-            minst.push(val);
-    }
-    
-    public void pop() {
-        if(getMin()==st.pop())
-            minst.pop();
-    }
-    
-    public int top() {
-        return st.peek();
-    }
-    
-    public int getMin() {
-        return minst.peek();
-    }
-}
-
-class MinStack { // #2
+class MinStack { // #1 || Naive
     private ArrayList<Integer> array = new ArrayList<>();
     private PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
 
@@ -53,3 +25,44 @@ class MinStack { // #2
         return minHeap.peek();}
 }
 
+// #2 || O(1) 
+class MinStack {
+
+    Stack<Integer> stk;
+    Stack<Integer> monoStk;
+    
+    public MinStack() {
+        stk = new Stack<>();
+        monoStk = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stk.push(val);
+        if(monoStk.size() < 1 || val < monoStk.peek())
+            monoStk.push(val);
+        else
+            monoStk.push(monoStk.peek());
+    }
+    
+    public void pop() {
+        monoStk.pop();
+        stk.pop();
+    }
+    
+    public int top() {
+        return stk.peek();
+    }
+    
+    public int getMin() {
+        return monoStk.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
